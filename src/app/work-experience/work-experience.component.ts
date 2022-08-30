c5362fce88c228bf2ff46dade9c454e9d4a7a13e
FhatimaReyes <fhareal10@gmail.com>
2022-08-30 13:18:33 -0500

    Create work_experience #2

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+  workExperience : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
-  }
+  
 
+ let work1 = {
+  fecha: "2017-2019",
+  ubicacion: "Córdoba, Ver",
+  puesto: "Waiter", 
+  empresa: "Banquetería",
+  logros: [
+        {descripcion: "Organizador de eventos"},
+        {descripcion: "Colaboraciones digitales"}
+  ]
+ };
+
+ let work2 = {
+    fecha: "2019-2020",
+    ubicacion: "Córdoba, Ver",
+    puesto: "Home Office", 
+    empresa: "Amazon",
+    logros: [
+      {descripcion: "Diseño de interfaces"},
+      {descripcion: "Colaboraciones digitales"}
+]
+   };
+
+
+ let work3 = {
+    fecha: "2020-2022",
+    ubicacion: "Córdoba, Ver",
+    puesto: "Waiter", 
+    empresa: "Granero Steak",
+    logros: [
+      {descripcion: "Colaboraciones proactivas"},
+      {descripcion: "Servicio al cliente"}
+]
+   };
+
+  this.workExperience.push(work1);
+  this.workExperience.push(work2);
+  this.workExperience.push(work3);
+  }
 }
