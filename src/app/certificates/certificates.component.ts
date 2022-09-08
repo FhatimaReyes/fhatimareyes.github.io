a3738cc6d903d4b697282e8d744f79f706bfc067
FhatimaReyes <fhareal10@gmail.com>
2022-09-08 16:09:08 -0500

    create branch certificates #14

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CertificatesComponent implements OnInit {
 
+  certificate : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
+    let certificate1 = {
+      certificado:"Diploma 6º Semestre Bachillerato",
+      fecha:"2020"
+    };
+  
+    let certificate2 = {
+      certificado:"Certificado auxiliar Contaduría",
+      fecha:"2020"
+    };
+  
+    let certificate3 = {
+      certificado:"Certificado Taller Informática",
+      fecha:"2021"
+    };
+    
+    this.certificate.push(certificate1);
+    this.certificate.push(certificate2);
+    this.certificate.push(certificate3);
+    //this.workExperience.push(work4);
   }
 
 }
